6bd73602f8d7250db681c39952ef290d5bb84a77
Alex Hernandez <alexh385@gmail.com>
2019-02-25 00:41:39 -0500

    Add test for CommentList inside App component

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+
+// Components
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
     const wrappedComponent = shallow(<App />);
 
     expect(wrappedComponent.find(CommentBox).length).toEqual(1);
+});
+
+it('shows a comment list', () => {
+    const wrappedComponent = shallow(<App />);
+
+    expect(wrappedComponent.find(CommentList).length).toEqual(1);
 });
