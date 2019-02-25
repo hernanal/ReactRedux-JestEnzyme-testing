ef4a0ef7ad1b76233358baec92488f5b4ef904e6
Alex Hernandez <alexh385@gmail.com>
2019-02-25 01:34:47 -0500

    Add beforeEach and afterEach, impl full DOM rendering, and refactor

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { mount } from 'enzyme';
 
 import CommentBox from 'components/CommentBox';
 
-it('has a text area and a button', () => {});
+let wrappedComponent;
+
+beforeEach(() => {
+    // Full DOM rendering
+    wrappedComponent = mount(<CommentBox />);
+});
+
+afterEach(() => {
+    wrappedComponent.unmount();
+});
+
+it('has a text area and a button', () => {
+    expect(wrappedComponent.find('textarea').length).toEqual(1);
+    expect(wrappedComponent.find('button').length).toEqual(1);
+});
