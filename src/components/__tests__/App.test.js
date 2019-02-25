bd2e6abcb896a2bdc6a4c87b8c4397c838d42d9b
Alex Hernandez <alexh385@gmail.com>
2019-02-25 00:50:47 -0500

    Refactor App test suite and implement beforeEach function

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -6,14 +6,16 @@ import App from 'components/App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-it('shows a comment box', () => {
-    const wrappedComponent = shallow(<App />);
+let wrappedComponent;
 
+beforeEach(() => {
+    wrappedComponent = shallow(<App />);
+});
+
+it('shows a comment box', () => {
     expect(wrappedComponent.find(CommentBox).length).toEqual(1);
 });
 
 it('shows a comment list', () => {
-    const wrappedComponent = shallow(<App />);
-
     expect(wrappedComponent.find(CommentList).length).toEqual(1);
 });
