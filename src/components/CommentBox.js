a1f05308a4570772645616603ddf73f8716b3dc4
Alex Hernandez <alexh385@gmail.com>
2019-02-25 01:14:19 -0500

    Add state, and event handlers for onChange and submit, to CommentBox

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+
+    state = {
+        comment: ''
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            comment: event.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        // prevent page from reloading
+        event.preventDefault();
+
+        // TODO: Call an action creator and save comment
+
+        this.setState({
+            comment: ''
+        });
+    };
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h4>Add Comment</h4>
-                <textarea />
+                <textarea onChange={this.handleChange} value={this.state.comment} />
                 <div>
                     <button>Submit Comment</button>
                 </div>
