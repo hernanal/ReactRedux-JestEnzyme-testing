5a87635ca4f5ee701c2833dcdfb64ee89a465a27
Alex Hernandez <alexh385@gmail.com>
2019-03-03 05:16:47 -0500

    Add async await to fetchComments to allow promise to resolve before dispatching action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,22 @@
-import axios from 'axios';
-import { SAVE_COMMENT } from 'actions/types';
-import { FETCH_COMMENTS } from './types';
+import axios from "axios";
+import { SAVE_COMMENT } from "actions/types";
+import { FETCH_COMMENTS } from "./types";
 
 export function saveComment(comment) {
-    return {
-        type: SAVE_COMMENT,
-        comment
-    };
+  return {
+    type: SAVE_COMMENT,
+    comment
+  };
 }
 
-export function fetchComments() {
-    const comments = axios.get('http://jsonplaceholder.typicode.com/comments');
+export async function fetchComments() {
+  const comments = await axios.get("http://jsonplaceholder.typicode.com/comments");
 
-    return {
-        type: FETCH_COMMENTS,
-        comments
-    }
+  return {
+    type: FETCH_COMMENTS,
+    comments
+  };
+    // return (dispatch, getState) => {
+    //     dispatch({ type: FETCH_COMMENTS, comments });
+    // }
 }
