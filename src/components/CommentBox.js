f45e63a535e43af57e13c8485449c26c0403c1b2
Alex Hernandez <alexh385@gmail.com>
2019-03-03 05:18:29 -0500

    Add button to fetch the  comments and refactor test case

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,39 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from 'actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class CommentBox extends Component {
+  state = {
+    comment: ""
+  };
 
-    state = {
-        comment: ''
-    };
+  handleChange = event => {
+    this.setState({
+      comment: event.target.value
+    });
+  };
 
-    handleChange = (event) => {
-        this.setState({
-            comment: event.target.value
-        });
-    };
+  handleSubmit = event => {
+    // prevent page from reloading
+    event.preventDefault();
 
-    handleSubmit = (event) => {
-        // prevent page from reloading
-        event.preventDefault();
-
-        this.props.saveComment(this.state.comment);
-        this.setState({
-            comment: ''
-        });
-    };
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <h4>Add Comment</h4>
-                <textarea onChange={this.handleChange} value={this.state.comment} />
-                <div>
-                    <button>Submit Comment</button>
-                </div>
-            </form>
-        );
-    }
+    this.props.saveComment(this.state.comment);
+    this.setState({
+      comment: ""
+    });
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
+    );
+  }
 }
 
-export default connect(null, actions)(CommentBox);
+export default connect(
+  null,
+  actions
+)(CommentBox);
