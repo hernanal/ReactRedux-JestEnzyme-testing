14f71eea9682ed18c4d9067800efa42d2da0161e
Alex Hernandez <alexh385@gmail.com>
2019-03-03 05:12:50 -0500

    Apply redux-thunk to store

--- src/Root.js
+++ src/Root.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxPromise from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 import reducers from 'reducers';
  
 export default ({ children, initialState = {} }) => {
     const store = createStore(
         reducers,
         initialState,
-        applyMiddleware(reduxPromise)
+        applyMiddleware(reduxPromise, ReduxThunk)
     );
     return (
         <Provider store={store} >
