5bdd391d4ad2dadb71dd34831b8b221d151b677f
Alex Hernandez <alexh385@gmail.com>
2019-03-03 03:49:11 -0500

    Add fetchComments action creator and type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
+import axios from 'axios';
 import { SAVE_COMMENT } from 'actions/types';
+import { FETCH_COMMENTS } from './types';
 
 export function saveComment(comment) {
     return {
         type: SAVE_COMMENT,
         comment
     };
+}
+
+export function fetchComments() {
+    const comments = axios.get('http://jsonplaceholder.typicode.com/comments');
+
+    return {
+        type: FETCH_COMMENTS,
+        comments
+    }
 }
